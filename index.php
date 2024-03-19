81f451702f1f6c98119035ac31f6d214529f5131
emanuele-pogliari <manu.pogliari@gmail.com>
2024-03-19 15:47:30 +0100

    basic logic function created

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+$pwdLength = $_GET['length'];
+
+function generatePassword($pwdLength)
+{
+    $pwd = "";
+    for ($i = 0; $i < $pwdLength; $i++) {
+        $pwd .= chr(rand(33, 126));
+    }
+    return $pwd;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +22,13 @@
 </head>
 
 <body>
+
+    <form method="GET">
+        <input type="number" name="length" placeholder="Enter the length of your password to be generated">
+        <button type="submit">Generate</button>
+        <p><?php echo generatePassword($pwdLength) ?></p>
+
+    </form>
 
 
 
