193e9dd51e81b4db39c04660026a03e73aa3540c
emanuele-pogliari <manu.pogliari@gmail.com>
2024-03-19 16:45:22 +0100

    milestone 2 completed

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-$pwdLength = $_GET['length'];
-
-function generatePassword($pwdLength)
-{
-    $pwd = "";
-    for ($i = 0; $i < $pwdLength; $i++) {
-        $pwd .= chr(rand(33, 126));
-    }
-    return $pwd;
-}
+include './partials/function.php';
 
 ?>
 
---
+++ partials/function.php
@@ -0,0 +1,11 @@
+<?php
+$pwdLength = $_GET['length'];
+
+function generatePassword($pwdLength)
+{
+    $pwd = "";
+    for ($i = 0; $i < $pwdLength; $i++) {
+        $pwd .= chr(rand(33, 126));
+    }
+    return $pwd;
+}
